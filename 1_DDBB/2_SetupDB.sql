ef45141ae81898cd54086b6e21c653c90d03c080
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-09 21:53:18 -0500

    Add: Scripts de Setup y Acciones a realizar en comentarios
    Solo comente las ideas y scripts que voy a requerir, no hay ni un script escrito solo su header de que se ralizara, junto con que tablas utilizare

---
+++ 1_DDBB/2_SetupDB.sql
@@ -0,0 +1,28 @@
+
+-- Create Tipo Empleado
+
+
+-- Create Admin User
+-- Jefe 
+
+-- Create Product Categories
+
+
+-- =================================
+-- Triggers:
+
+-- Update Product:Stock
+-- Dispadado por: Inventario:Quantity
+-- Razon: Ingreso de cierta cantidad de productos al Inventario
+
+
+
+-- Update Product:Stock
+-- Dispadado por: Product_Items:Quantity
+-- Razon: 
+
+
+
+
+
+
---
+++ 1_DDBB/3_DB_Scripts.sql
@@ -0,0 +1,79 @@
+-- Actions --
+
+-- Login
+-- By: Admin and Employees
+-- Check if match on empleado
+-- Check if it's active
+
+
+
+-- Create Empleado
+-- By: Admin
+
+
+
+-- Ver lista de empleados
+-- By: Admin
+
+
+
+-- Ver lista de empleados activos
+-- By: Admin
+
+
+
+-- Desactivar empleado Activo
+-- By: Admin
+-- Solo ponerle la fecha EndDate de hoy
+-- TODO: Pensar en Agregar el Activar un empleado?
+
+
+-- Registrar Productos
+-- By: Admin
+-- Requiere:
+-- Registrar el ProductPrices TBL
+-- Escoger ProductCategory TBL
+
+
+
+-- Actualizar ProductPrices
+-- By: Admin
+
+
+-- Desactivar productos en venta
+-- Productos: Activo=0
+-- By: Admin
+
+
+-- Ver todo los productos Activos
+-- Con Stock > 0
+-- By: Empleado
+-- Extra: Mostrar categoria
+
+
+-- Venta de productos
+--------------------------------
+-- Agregar productos al carrito
+-- TODO: Crear la tabla carrito
+-- By: Empleado
+
+
+-- Transaccion de venta de productos
+-- By: Empleado
+-- Requiere agregar:
+-- OrderReceipt TBL & Product_Items TBL
+-- Datos requeridos:
+-- TBLs: ProductPrices, Productos, Empleado
+
+
+-- Ver ordenes que ha realizado
+-- By: Empleado (el mismo)
+-- Requiere:
+-- TBL Order Receipt
+
+
+-- Ver Items de las ordenes que realizo
+-- By: Empleado (el mismo)
+-- Requiere: (Order Receipt ID)
+-- TBL Product_Items
+

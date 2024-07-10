1f5eec63d3faf915b0dd7b5f30911c4308c51ef2
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-10 06:32:07 -0500

    Update: Add unos SProcedures & setup scripts
    Solo modifique unos scripts para que fueran Store Procedure, los cuales tienen que ver con el login y encriptacion

--- 1_DDBB/2_SetupDB.sql
+++ 1_DDBB/2_SetupDB.sql
@@ -1,33 +1,61 @@
+-- =======================================================================
 USE DB_CAPS
+-- =======================================================================
 -- Create Tipo Empleado
 INSERT INTO TipoEmpleado
 VALUES
-('ADMINISTRADOR'),('EMPLEADO')
+('ADMINISTRADOR'),('EMPLEADO')--select * from TipoEmpleado-- =======================================================================
 -- Create Admin User
 -- Jefe
-DECLARE @EmployeeType INT
-DECLARE @Turno VARCHAR(50)
-DECLARE @PERSONAL VARCHAR(50)
-INSERT INTO dbo.Empleado
-(IdEmpleado, Nombre, ApPaterno, ApMaterno, Contrasena, EmployeeType)
-VALUES
-('ADM-567890','Fernando', 'Fernandez', 'Rios','12345', 1)
---,('EMP-567890', 'Angel A.', 'Galarza', 'Chavez'	,'12345',2)
-SET @PERSONAL ='ADM-567890'
-SET @Turno ='Completo'
+CREATE PROCEDURE InsertarEmpleado --@IdEmpleado CHAR(10),
+    @Nombre NVARCHAR(100),  @ApPaterno NVARCHAR(100), @ApMaterno NVARCHAR(100), @Contrasena VARCHAR(30), @EmployeeType INT,
+	@NuevoID CHAR(10) OUTPUT
+AS
+BEGIN	
+    SET NOCOUNT ON;
+	DECLARE @IdEmpleado CHAR(10)		DECLARE @CountEmployees INT;	DECLARE @EmpTypeNamePrefix VARCHAR(3);
+    SELECT @CountEmployees = COUNT(*)+1 FROM dbo.Empleado WHERE EmployeeType = @EmployeeType;	--PRINT @CountEmployees
+    SELECT TOP 1 @EmpTypeNamePrefix = LEFT(EmpTypeName, 3) FROM dbo.TipoEmpleado WHERE IdEmployeeType = @EmployeeType
+	SET @IdEmpleado = CONCAT(@EmpTypeNamePrefix, '-', FORMAT(@CountEmployees, '000000')); --PRINT @elID    
+	DECLARE @PasswordHash VARBINARY(MAX);		SET @PasswordHash = HASHBYTES('SHA2_256', @Contrasena);
 
-INSERT INTO dbo.Empleado_Activo
-(IdEmpleado, StartDate, EndDate, Turno)
-VALUES(@PERSONAL, GETDATE(), null, @Turno)
+    INSERT INTO dbo.Empleado
+        (IdEmpleado, Nombre, ApPaterno, ApMaterno, Contrasena, EmployeeType)
+    VALUES
+        (@IdEmpleado, @Nombre, @ApPaterno, @ApMaterno, @PasswordHash, @EmployeeType);
+	
+	SET @NuevoID = @IdEmpleado
+END;
+GO
+--DECLARE @ID CHAR(10);
+--EXEC InsertarEmpleado --@IdEmpleado = 'ADM-567890',
+--    @Nombre = 'Fernando', @ApPaterno = 'Fernandez', @ApMaterno = 'Rios', @Contrasena = '12345',
+--    @EmployeeType = 1, @NuevoID = @ID OUTPUT
+--SELECT @ID 
+--,('EMP-567890', 'Angel A.', 'Galarza', 'Chavez'	,'12345',2)-- =======================================================================
+CREATE PROCEDURE ActivarUsuario
+    @IdEmpleado CHAR(10),
+    @Turno VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO dbo.Empleado_Activo
+	(IdEmpleado, StartDate, EndDate, Turno)
+	VALUES(@IdEmpleado, GETDATE(), null, @Turno)
+END
 
+EXEC ActivarUsuario
+    @IdEmpleado ='ADM-567890',
+	@Turno ='Completo'
+
+-- =======================================================================
 -- Create Product Categories
 INSERT INTO dbo.ProductCategory
 (CategoryName)--MAX:100
 VALUES('Completo')
 GO
--- =================================
+-- =======================================================================
 -- Triggers:
-
+-- =======================================================================
 -- Update Product:Stock
 -- Dispadado por: Inventario:Quantity
 -- Razon: Ingreso de cierta cantidad de productos al Inventario
@@ -43,10 +71,10 @@ BEGIN
 
 	UPDATE [dbo].[Productos]
 	SET Stock = Stock + @CANTIDAD
-	WHERE IdProducto=@SKU--SKU=@SKU
+	WHERE IdProducto=@SKU	--SKU=@SKU
 END
 GO
-
+-- -----------------------------------------------------------------------
 -- Update Product:Stock
 -- Dispadado por: Product_Items:Quantity
 -- Razon: Venta de cierta cantidad de productos.

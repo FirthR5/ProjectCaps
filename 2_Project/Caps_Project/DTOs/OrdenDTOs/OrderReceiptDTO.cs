24b3ef6ebb04ebed1cf7e53a67c186743d64bd26
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-10 20:43:05 -0500

    Add: Orden DTOs archivos
    Se agrego el carrito DTO

---
+++ 2_Project/Caps_Project/DTOs/OrdenDTOs/OrderReceiptDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Caps_Project.DTOs.OrdenDTOs
+{
+    public class OrderReceiptDTO
+    {
+        public int OrderId { get; set; }
+
+        public decimal TotalPaid { get; set; }
+
+        public DateTime OrderDate { get; set; }
+    }
+}

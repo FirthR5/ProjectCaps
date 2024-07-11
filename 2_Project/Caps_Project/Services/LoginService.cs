16c6c39192088e45258b0836779a2df10a913059
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-10 20:39:18 -0500

    Feat/Fix: Login Service funcionando (full SP)
    Dos funciones de Login (verificacion de credenciales y si esta activo) funcionando.

--- 2_Project/Caps_Project/Services/LoginService.cs
+++ 2_Project/Caps_Project/Services/LoginService.cs
@@ -1,15 +1,50 @@
-﻿using Caps_Project.Migrations;
+﻿using Caps_Project.DTOs.LoginDTOs;
+using Caps_Project.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace Caps_Project.Services
 {
     public class LoginService: BaseService
     {
         public LoginService(DbCapsContext context ) : base(context) { }
-        public void prueba()
-        {
-            var x = context.Empleados.ToList();
-            context.SaveChanges();
-        }
 
+        public async Task<int> VerificarCredenciales(LoginDTO credenciales)
+        {
+            //Input Parameter
+            var IdEmpleadoParameter = new SqlParameter("IdEmpleado", credenciales.IdEmpleado);
+            var ContrasenaParameter = new SqlParameter("Contrasena", credenciales.Contrasena);
+            //Output Parameter
+            var correctoParam = new SqlParameter
+            {
+                ParameterName = "Correcto", SqlDbType = SqlDbType.Int,
+                Direction = ParameterDirection.Output
+            };
+
+            context.Database.ExecuteSqlRaw("EXEC VerificarCredenciales @IdEmpleado,@Contrasena, @Correcto OUTPUT",
+                IdEmpleadoParameter, ContrasenaParameter, correctoParam);
+
+            int Exitoso = (int)correctoParam.Value;
+            return Exitoso;
+        }
+        public async Task<int> VerificarUsuarioActivo(string idEmpleado)
+        {
+            var idEmpleadoParam = new SqlParameter("@IdEmpleado", idEmpleado);
+            var countParam = new SqlParameter
+            {
+                ParameterName = "@Count",
+                SqlDbType = System.Data.SqlDbType.Int,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            await context.Database.ExecuteSqlRawAsync(
+                "EXEC @Count = VerificarUsuarioActivo @IdEmpleado",
+                countParam,
+                idEmpleadoParam
+            );
+
+            return (int)countParam.Value;
+        }
     }
 }

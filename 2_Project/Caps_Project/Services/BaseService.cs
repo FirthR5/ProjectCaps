c5494e3b8249defd50ac735f348c34cdb36a18b9
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-11 16:36:17 -0500

    Feat: BaseService nuevo Ctor integra IMapper+Context

--- 2_Project/Caps_Project/Services/BaseService.cs
+++ 2_Project/Caps_Project/Services/BaseService.cs
@@ -1,14 +1,21 @@
-﻿using Caps_Project.Models;
+﻿using AutoMapper;
+using Caps_Project.Models;
 
 namespace Caps_Project.Services
 {
     public class BaseService
     {
         protected readonly DbCapsContext context;
+        protected readonly IMapper mapper;
 
         public BaseService(DbCapsContext context )
         {
             this.context = context;
+        }
+        public BaseService(DbCapsContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
         }
     }
 }

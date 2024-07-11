4ed6efaf483148c29a9a3004755cc276e818e91a
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-11 17:23:13 -0500

    Feat: Http,Inventario Controlador: add IMapper+Servicio Metodos funcionando p1
    > Http
    >> Update:
    Prueba de funciones en Controller
    > Inventario Controller:
    >> Integra:
    IMapper y Context en Ctor.
    >> Add: Metodos funcionando:
    Funcionando probado con Http en Debugging
    ListaInventario, RegistraNuevoInventario, AlmacenaProducto, GetListProdCat.
    >> Add: Metodos Estructura:
    DisableProduct,ActualizarPrecio

--- 2_Project/Caps_Project/Controllers/InventarioController.cs
+++ 2_Project/Caps_Project/Controllers/InventarioController.cs
@@ -1,83 +1,138 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Caps_Project.DTOs.InventarioDTOs;
+using Caps_Project.Models;
+using Caps_Project.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Caps_Project.Controllers
 {
     public class InventarioController : Controller
     {
+        private readonly DbCapsContext contexto;
+        private readonly IMapper mapper;
+        public InventarioController(DbCapsContext context, IMapper mapper)
+        {
+            this.contexto = context;
+            this.mapper = mapper;
+        }
+
         // GET: InventarioController
         public ActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// Obtener lista de los productos activos del inventario
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> ListaInventario()
+        {
+            InventarioService Serv_Inventario = new InventarioService(contexto);
+            var listaProductos = await Serv_Inventario.ListProductos();
 
-        // GET: InventarioController/Details/5
+            return View(listaProductos);
+        }
+
+        // Registrar un Producto
+        // TODO: Agregar redirecciones
+        /// <summary>
+        /// Registrar productos
+        /// </summary>
+        /// <param name="nuevoProducto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult> RegistraNuevoProducto([FromBody] NuevoProductoDTO nuevoProducto)
+        {
+
+            try
+            {
+                // Productos + Product Prices
+                InventarioService Serv_Inv = new InventarioService(contexto, mapper);
+                bool queryExitoso = await Serv_Inv.RegistrarProducto(nuevoProducto);
+                if ( !queryExitoso)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // Almacenar Producto
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult> AlmacenaProducto([FromBody]NewProductoPriceDTO newProductPrice)
+        {
+            try
+            {
+                InventarioService Serv_Inv = new InventarioService(contexto, mapper);
+                bool executionSuccessful = await Serv_Inv.InsertOrUpdateProductPrices(newProductPrice);
+                if (!executionSuccessful)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return RedirectToAction(nameof(Index));
+            }//
+            catch
+            {
+                return View();
+            }
+        }
+
+        // Actualiza Precio de Producto
+        /// <summary>
+        /// Actualizar el precio del producto
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ActualizarPrecio(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // Deshabilita Producto
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DisableProduct(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // ????
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: InventarioController/Create
-        public ActionResult Create()
+        public async Task<ActionResult> GetListProdCat()
         {
-            return View();
-        }
-
-        // POST: InventarioController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: InventarioController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: InventarioController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: InventarioController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: InventarioController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            InventarioService Serv_Inv = new InventarioService(contexto);
+            List<ProductCategory> lista = await Serv_Inv.ListProductCategorias();
+            var listaCategorias = mapper.Map<List<ProdCategoryDTO>>(lista);
+            return Ok(listaCategorias);
         }
     }
 }

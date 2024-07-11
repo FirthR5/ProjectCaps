9e0a3fb363d4b5cd19567203184ce2377c655246
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-10 20:34:05 -0500

    Fix: Login SP
    Arreglando los SP de Login, y aparte haciendo pequeños cambios con el formato(para que sea mas leible el script)

--- 1_DDBB/2_SetupDB.sql
+++ 1_DDBB/2_SetupDB.sql
@@ -1,18 +1,17 @@
 -- =======================================================================
 USE DB_CAPS
 -- =======================================================================
--- Create Tipo Empleado
+-- (0) Create Tipo Empleado
 INSERT INTO TipoEmpleado
 VALUES
 ('ADMINISTRADOR'),('EMPLEADO')--select * from TipoEmpleado-- =======================================================================
--- Create Admin User
--- Jefe
+-- (3) I. Create Admin User
+-- By: Admin. Inserta aqui al: Jefe
 CREATE PROCEDURE InsertarEmpleado --@IdEmpleado CHAR(10),
     @Nombre NVARCHAR(100),  @ApPaterno NVARCHAR(100), @ApMaterno NVARCHAR(100), @Contrasena VARCHAR(30), @EmployeeType INT,
 	@NuevoID CHAR(10) OUTPUT
 AS
 BEGIN	
-    SET NOCOUNT ON;
 	DECLARE @IdEmpleado CHAR(10)		DECLARE @CountEmployees INT;	DECLARE @EmpTypeNamePrefix VARCHAR(3);
     SELECT @CountEmployees = COUNT(*)+1 FROM dbo.Empleado WHERE EmployeeType = @EmployeeType;	--PRINT @CountEmployees
     SELECT TOP 1 @EmpTypeNamePrefix = LEFT(EmpTypeName, 3) FROM dbo.TipoEmpleado WHERE IdEmployeeType = @EmployeeType
@@ -20,19 +19,20 @@ BEGIN
 	DECLARE @PasswordHash VARBINARY(MAX);		SET @PasswordHash = HASHBYTES('SHA2_256', @Contrasena);
 
     INSERT INTO dbo.Empleado
-        (IdEmpleado, Nombre, ApPaterno, ApMaterno, Contrasena, EmployeeType)
+	(IdEmpleado, Nombre, ApPaterno, ApMaterno, Contrasena, EmployeeType)
     VALUES
         (@IdEmpleado, @Nombre, @ApPaterno, @ApMaterno, @PasswordHash, @EmployeeType);
 	
 	SET @NuevoID = @IdEmpleado
 END;
 GO
---DECLARE @ID CHAR(10);
---EXEC InsertarEmpleado --@IdEmpleado = 'ADM-567890',
---    @Nombre = 'Fernando', @ApPaterno = 'Fernandez', @ApMaterno = 'Rios', @Contrasena = '12345',
---    @EmployeeType = 1, @NuevoID = @ID OUTPUT
---SELECT @ID 
---,('EMP-567890', 'Angel A.', 'Galarza', 'Chavez'	,'12345',2)-- =======================================================================
+DECLARE @ID CHAR(10);
+EXEC InsertarEmpleado --@IdEmpleado = 'ADM-567890',
+    @Nombre = 'Fernando', @ApPaterno = 'Fernandez', @ApMaterno = 'Rios', @Contrasena = '12345',
+    @EmployeeType = 1, @NuevoID = @ID OUTPUT
+SELECT @ID -- =======================================================================
+-- (3) II. Activar Usuario
+-- By: Admin
 CREATE PROCEDURE ActivarUsuario
     @IdEmpleado CHAR(10),
     @Turno VARCHAR(50)
@@ -44,11 +44,11 @@ BEGIN
 END
 
 EXEC ActivarUsuario
-    @IdEmpleado ='ADM-567890',
+    @IdEmpleado ='ADM-000001',
 	@Turno ='Completo'
 
 -- =======================================================================
--- Create Product Categories
+-- (12) Create Product Categories
 INSERT INTO dbo.ProductCategory
 (CategoryName)--MAX:100
 VALUES('Completo')
--- 1_DDBB/3_DB_Scripts.sql
+++ 1_DDBB/3_DB_Scripts.sql
@@ -14,36 +14,41 @@ USE DB_CAPS
 -- Check if match on empleado
 -- Check if it's active
 
--- 1. Match Empleado
-CREATE PROCEDURE VerificarCredenciales
+-- (1) I. Match Empleado
+Alter PROCEDURE VerificarCredenciales
     @IdEmpleado CHAR(10),
-    @Contrasena VARCHAR(30)
+    @Contrasena VARCHAR(30), 
+	@Correcto INT OUTPUT
 AS
 BEGIN
-    SELECT COUNT(1) AS Cantidad
+    SET @Correcto = (SELECT COUNT(1) AS Cantidad
     FROM dbo.Empleado
     WHERE IdEmpleado = @IdEmpleado
-    AND Contrasena = HASHBYTES('SHA2_256', @Contrasena);
+    AND Contrasena = HASHBYTES('SHA2_256', @Contrasena));
 END;
-EXEC VerificarCredenciales 
-	@IdEmpleado ='ADM-567890',
-	@Contrasena = '12345';
 
--- 2. Checar si esta activo
+DECLARE @EsCorrecto INT;
+EXEC VerificarCredenciales 
+    @IdEmpleado ='ADM-000001', @Contrasena = '12345', 
+    @Correcto = @EsCorrecto OUTPUT;
+SELECT @EsCorrecto;
+-----------------------
+-- (1) II. Checar si esta activo
 CREATE PROCEDURE VerificarUsuarioActivo
-    @IdEmpleado CHAR(10)
+    @IdEmpleado CHAR(10), @Activo INT OUT
 AS
 BEGIN
-	SELECT COUNT(1) FROM Empleado_Activo
+	SET @Activo = (SELECT COUNT(1) FROM Empleado_Activo
 	WHERE IdEmpleado=@IdEmpleado AND
-	EndDate IS NULL
+	EndDate IS NULL)
 END
-EXEC VerificarUsuarioActivo @IdEmpleado ='ADM-567890'
+DECLARE @EstaActivo INT
+EXEC VerificarUsuarioActivo @IdEmpleado ='ADM-000001', @Activo = @EstaActivo  OUTPUT
+SELECT @EstaActivo
 
---SELECT * FROM dbo.Empleado;SELECT * FROM dbo.Empleado_Activo
 -- =======================================================================
 
--- Ver Datos del usuario (El mismo usuario ve su informacion):
+-- (2) Ver Datos del usuario (El mismo usuario ve su informacion):
 -- By: Todos los empleados, Admin
 -- TODO: Probar View despues
 CREATE VIEW vw_Datos_Usuario
@@ -55,16 +60,23 @@ INNER JOIN dbo.TipoEmpleado TE ON E.EmployeeType = TE.IdEmployeeType
 
 -- =======================================================================
 
--- Create Empleado
+-- (3) I. Create Empleado
 -- By: Admin
 DECLARE @ID CHAR(10);
 EXEC InsertarEmpleado --@IdEmpleado = 'ADM-567890',
-    @Nombre = 'Angel A.', @ApPaterno = 'Galarza', @ApMaterno = 'Chavez', @Contrasena = '12345',
+    @Nombre = 'Angel A.', @ApPaterno = 'Galarza', @ApMaterno = 'Chavez', 
+	@Contrasena = '12345',
     @EmployeeType = 2, @NuevoID = @ID OUTPUT
 SELECT @ID 
 
+-- (3) II. Activar Usuario
+-- By: Admin
+EXEC ActivarUsuario
+    @IdEmpleado ='ADM-000001',
+	@Turno ='Completo'
 
--- Ver lista de empleados
+
+-- (4) Ver lista de empleados
 -- By: Admin
 -- TODO: Probar View despues
 SELECT E.IdEmpleado, (Nombre + ' ' + ApPaterno + ' ' + ApMaterno) AS NombreCompleto,
@@ -81,7 +93,7 @@ OUTER APPLY (
 ) EA;
 --SELECT * FROM dbo.Empleado
 
--- Ver lista de empleados activos
+-- (5) Ver lista de empleados activos
 -- By: Admin
 -- TODO: Probar View despues
 SELECT E.IdEmpleado, (Nombre + ' ' + ApPaterno + ' ' + ApMaterno) AS NombreCompleto,
@@ -92,7 +104,7 @@ WHERE IdEmpleado IN (SELECT IdEmpleado FROM Empleado_Activo WHERE EndDate IS NUL
 
 -- SELECT * FROM Empleado_Activo
 
--- Desactivar empleado Activo
+-- (6) Desactivar empleado Activo
 -- By: Admin
 -- Solo ponerle la fecha EndDate de hoy
 -- TODO: Pensar en Agregar el Activar un empleado?
@@ -107,7 +119,7 @@ WHERE idEmpleado = @IdEmpleado
 -- =======================================================================
 
 
--- Registrar Productos
+-- (7) Registrar Productos
 -- By: Admin
 -- Requiere:
 -- Registrar el ProductPrices TBL
@@ -118,15 +130,15 @@ DECLARE @Activo BIT;
 SET @IdProdCategory = 50;
 SET @Activo = 1;
 
--- Check if product exists en Productos
+-- (7) I. Check if product exists en Productos
 SELECT COUNT(1) FROM Productos WHERE IdProducto = 1
 
--- Registro el Producto si no existe
+-- (7) II.Registro el Producto si no existe
 Insert Into Productos(ProductName, Stock, IdProdCategory, Descripcion, Activo)
 VALUES ('name', 0, @IdProdCategory, 'Descripcion Lol', @Activo)
 
 
--- Insertar y Actualizar ProductPrices
+-- (7) III. Insertar y Actualizar ProductPrices
 -- By: Admin
 CREATE PROCEDURE sp_InsertOrUpdateProductPrices
     @ProductID INT,
@@ -155,7 +167,7 @@ END;
 
 
 -- =======================================================================
--- Ingreso de Inventario
+-- (8) I. Ingreso de Inventario
 DECLARE @IdEmpleado CHAR(10);
 DECLARE @IdProduct INT;
 DECLARE @Quantity INT;
@@ -168,7 +180,7 @@ VALUES (@IdProduct, GETDATE(), @Quantity, @IdEmpleado);
 
 -- =======================================================================
 
--- Desactivar productos en venta
+-- (8) II. Desactivar productos en venta
 -- Productos: Activo=0
 -- By: Admin
 DECLARE @IdProducto INT;
@@ -179,7 +191,7 @@ SET Activo = 0
 WHERE IdProducto = @IdProducto
 
 
--- Ver todo los productos Activos
+-- (9) Ver todo los productos Activos
 -- Con Stock > 0
 -- By: Empleado
 -- Extra: Mostrar categoria
@@ -191,35 +203,37 @@ WHERE Stock > 0 AND Activo = 1
 -- Venta de productos
 --------------------------------
 
--- Agregar/Guardar productos al Carrito
+-- (10) Agregar/Guardar productos al Carrito
 -- (Add a TemporalTable for ItemsReceipt)
 -- By: Empleado
 -- IdItem, Quantity, TickerOrderId, ProductID
 DECLARE @OrderUUID UNIQUEIDENTIFIER
 SET @OrderUUID = NEWID();
-
+-- (10) I. Temp Carrito Guardar
 INSERT INTO TempCarrito (ProductPriceID, Quantity, OrderUUID, ProductID)
 VALUES (101, 2, @OrderUUID, 5001);
 
 
--- Operacion Delete
+-- (10) II. Operacion Delete
 -- Eliminar todo los productos del carrito despues de realizar la compra
 -- By: Empleado
 DELETE TempCarrito WHERE OrderUUID = NEWID() -- TODO: Aqui poner el UUID 
 ---
 
---Lista del carrito para agregarlo al ProductItems
+-- (10) III. Lista del carrito para agregarlo al ProductItems
 -- By: Empleado
 DECLARE @OrderUUID UNIQUEIDENTIFIER
 SET @OrderUUID = 'UUID a Consultar';
 
 SELECT * FROM TempCarrito WHERE OrderUUID = @OrderUUID;
 
--- Select the last price of ProductPrices
+-- (10) IV. Select the last price of ProductPrices
 -- TODO:
+SELECT * FROM ProductPrices 
+WHERE EndDate = NULL AND ProductId = @ProductID
 
 
--- Transaccion de venta de productos
+-- (11) I. Transaccion de venta de productos
 -- By: Empleado
 -- Requiere agregar:
 -- OrderReceipt TBL & Product_Items TBL
@@ -238,14 +252,14 @@ DECLARE @OrderId INT;
 SET @OrderId = 1; -- Obtener el OrderID del Receipt
 SET @IdProduct = 1;
 
--- Insertar datos de TempCarrito
+-- (11) II. Insertar datos de TempCarrito
 Insert Into Product_Items(ProductID,ProductPriceID, Quantity, TickerOrderID)
 VALUES (@IdProduct,1,2,@OrderId)
 
 
 
 -- =======================================================================
--- Ver ordenes que ha realizado
+-- (12) I. Ver ordenes que ha realizado
 -- By: Empleado (el mismo)
 -- Requiere:
 -- TBL Order Receipt
@@ -255,7 +269,7 @@ SET @IdEmpleado = 'ADM-567890';
 SELECT * FROM OrderReceipt
 WHERE IdEmpleado = @IdEmpleado
 
--- Ver Items de las ordenes que realizo
+-- (11) II. Ver Items de las ordenes que realizo
 -- By: Empleado (el mismo)
 -- Requiere: (Order Receipt ID)
 -- TBL Product_Items
@@ -271,8 +285,8 @@ WHERE OrderId = @OrderId
 
 -- Extras
 -- Drop-Down
--- Ver Tipo Empleado
-SELECT * FROM TipoEmpleado-- Ver Product CategorySELECT * FROM ProductCategory-- Ver TurnosCREATE PROCEDURE sp_ListaDeTurnos
+-- (0) Ver Tipo Empleado
+SELECT * FROM TipoEmpleado-- (12) Ver Product CategorySELECT * FROM ProductCategory-- Ver TurnosCREATE PROCEDURE sp_ListaDeTurnos
 AS
 BEGIN
     DECLARE @Turnos TABLE (

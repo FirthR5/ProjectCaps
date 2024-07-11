3a1bdda1308572f5c15384e8fe0cf90745c5ce3b
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-10 20:41:36 -0500

    Add: DTO Inventario files
    Se agrego IngresoInventario y NewProductPrice DTOs

---
+++ 2_Project/Caps_Project/DTOs/InventarioDTOs/IngresoInventarioDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Caps_Project.DTOs.InventarioDTOs
+{
+    public class IngresoInventarioDTO
+    {
+        public int IdProduct { get; set; }
+        public string IdAdmin { get; set; } = null!;
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ 2_Project/Caps_Project/DTOs/InventarioDTOs/NewProductoPriceDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Caps_Project.DTOs.InventarioDTOs
+{
+    public class NewProductoPriceDTO
+    {
+        public int ProductId { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}

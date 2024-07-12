1f907411dab23809ac2b5399df17394f031865ee
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-12 16:21:18 -0500

    Feature: ProductosService add all functionality v1
    Metodos agregados:
    > DesactivarProducto
    > AgregarAlCarrito
    > EliminarItemDelCarrito
    > EditarItemCarrito
    > EliminarItemsTodos
    > TotalPagar
    > RegistrarOrden
    > VerOrdenesRealizadas

--- 2_Project/Caps_Project/Services/ProductoVenderService.cs
+++ 2_Project/Caps_Project/Services/ProductoVenderService.cs
@@ -1,5 +1,7 @@
 ﻿using Azure.Identity;
+using Caps_Project.DTOs.OrdenDTOs;
 using Caps_Project.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
 
@@ -8,51 +10,154 @@ namespace Caps_Project.Services
     public class ProductoVenderService : BaseService
     {
         public ProductoVenderService(DbCapsContext context) : base(context) { }
-        private void DesactivarProducto(int IdProducto)
+        /// <summary>
+        ///  Desactivar productos por ID
+        /// (Llamado por InventarioController)
+        /// </summary>
+        /// <param name="IdProducto"></param>
+        /// <returns></returns>
+        public async Task<bool> DesactivarProducto(int IdProducto)
         {
             var producto = context.Productos.FirstOrDefault(p => p.IdProducto == IdProducto);
 
             if (producto != null)
             {
                 producto.Activo = false;
-                context.SaveChangesAsync();
+                await context.SaveChangesAsync();
+                return true;
             }
             else
             {
-                // Manejar el caso en que el producto no exista, si es necesario
-                throw new Exception($"No se encontró el producto con IdProducto = {IdProducto}");
+                return false;
             }
         }
-        private void AgregarAlCarrito(TempCarrito carritoTmp)
+
+
+        #region Carrito Temporal
+        // TODO: Checar lo del UUID
+        /// <summary>
+        /// Agregar al Carrito
+        /// </summary>
+        /// <param name="carritoTmp"></param>
+        /// <returns></returns>
+        public async Task<bool> AgregarAlCarrito(ItemCarritoDTO itemCarritoDTO)
         {
-            context.TempCarritos.Add(carritoTmp);
-            context.SaveChangesAsync();
+            try
+            {
+                TempCarrito nuevoItem;
+                nuevoItem = mapper.Map<TempCarrito>(itemCarritoDTO);
+                
+                //nuevoItem.OrderUuid = TODO: Crear UUid
+                var productPrice = await context.ProductPrices
+                                    .FirstAsync(p => p.EndDate == null && p.ProductId == itemCarritoDTO.ProductId);
+                nuevoItem.ProductPriceId = productPrice.IdPrice;
+
+
+                context.TempCarritos.Add(nuevoItem);
+                context.SaveChangesAsync();
+            }
+            catch
+            {
+                return false;
+            }
+           
+            return true;
         }
-        private void EliminarUnDelCarrito(TempCarrito carritoTmp)
+
+
+        /// <summary>
+        /// Eliminar un Item del carrito de compras temporal
+        /// Mediante Id
+        /// </summary>
+        /// <param name="IdItem"></param>
+        /// <returns></returns>
+        public async Task<bool> EliminarItemDelCarrito(int IdItem)
         {
-            context.TempCarritos.Add(carritoTmp);
-            context.SaveChangesAsync();
+            try
+            {
+                var objeto = new TempCarrito { IdItem = IdItem };
+                context.TempCarritos.Attach(objeto);
+                context.TempCarritos.Remove(objeto);
+
+                await context.SaveChangesAsync();
+                return true;
+
+            }
+            catch { return false; }
         }
-        public async Task<int> EliminarItemsPorOrderUUID(Guid orderUUID)
+
+        // TODO: Revisar esto ya cuando tenga mi FrontEnd
+        /// <summary>
+        /// Editar cantidad de productos que ha escogido
+        /// </summary>
+        /// <param name="itemCarritoDTO"></param>
+        /// <returns></returns>
+        public async Task<bool> EditarItemCarrito(EditarCarritoDTO itemCarritoDTO)
         {
-            var itemsAEliminar = await context.TempCarritos.Where(tc => tc.OrderUuid == orderUUID).ToListAsync();
+            try
+            {
+                var itemEditar = await context.TempCarritos
+                                .FirstOrDefaultAsync(m => m.IdItem == itemCarritoDTO.IdItem);//TODO: OrderUUID si es que lo utilizo
+                itemEditar.Quantity = itemCarritoDTO.Quantity;
+                //nuevoItem = mapper.Map<TempCarrito>(itemCarritoDTO);
+
+                context.SaveChangesAsync();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // TODO: Evaluar despues si es bueno lo del UUID o Eliminar todos
+        /// <summary>
+        /// Eliminar todos los articulos del temporal
+        /// </summary>
+        /// <param name="orderUUID"></param>
+        /// <returns></returns>
+        public async Task<int> EliminarItemsTodos()//PorOrderUUID(Guid orderUUID)
+        {
+            var itemsAEliminar = await context.TempCarritos.ToListAsync();
+            //var itemsAEliminar = await context.TempCarritos.Where(tc => tc.OrderUuid == orderUUID).ToListAsync();
             context.TempCarritos.RemoveRange(itemsAEliminar);
             return await context.SaveChangesAsync();
         }
-        private decimal TotalPagar(List<ProductItem> productItems)
+        #endregion
+
+        #region Carrito Pagar
+        // TODO: Try-Catch
+        // TODO: Ver si funciona
+        private async Task<decimal> TotalPagar(List<ProductItem> productItems)
         {
-            // TODO: Funcionalidad
-            return 0;
+            decimal total = 0;  
+
+            // Get: Lista de todos los productos del carrito temporal
+            var ItemsCarritoTmp = await context.TempCarritos
+                .Select(m => new {m.IdItem, m.ProductPriceId}).ToListAsync();
+            // Obtener precio PrecioTotal
+            foreach (var precio in ItemsCarritoTmp){
+                total += context.ProductPrices.First(p => p.IdPrice == precio.ProductPriceId).UnitPrice;
+            }
+            return total;
         }
+
+                
+        // TODO: Evaluar si es mejor sacar los Items de la BD
         public async Task<int> RegistrarOrden(string idEmpleado, List<ProductItem> productItems )
         {
-            // TODO: Check this later, i think and i remember the Trigger will no work with this.
+            // TODO: Funcionalidad
+           
             using (var transaction = context.Database.BeginTransaction())
             {
                 try
                 {
+                    // Obtener precio PrecioTotal
+                    // Asignar precio total
+                    decimal totalPaid = await TotalPagar(productItems);
+
                     // Crear el OrderReceipt
-                    decimal totalPaid = TotalPagar(productItems);
                     var orderReceipt = new OrderReceipt
                     {
                         IdEmpleado = idEmpleado,
@@ -62,9 +167,14 @@ namespace Caps_Project.Services
                     context.OrderReceipts.AddAsync(orderReceipt);
                     await context.SaveChangesAsync();
 
+                     // =========================================================
+                     // Get: Lista de todos los productos del carrito temporal
+                     // =========================================================
+                    var ItemsCarritoTmp = await context.TempCarritos.ToListAsync();
+
+                    // Registrar productos Items (IdOrden a Items)
                     // Asignar el OrderId generado a los Product_Items
-                    foreach (var item in productItems)
-                    {
+                    foreach (var item in productItems){
                         item.TicketOrderId = orderReceipt.OrderId;
                     }
 
@@ -72,31 +182,75 @@ namespace Caps_Project.Services
                     await context.ProductItems.AddRangeAsync(productItems);
                     await context.SaveChangesAsync();
 
+                    // Eliminar todo los items del carrito
+                    int correcto = await EliminarItemsTodos();
+                    if (correcto <1){
+                        transaction.Rollback();
+                        return 0;
+                    }
+
+                    // TODO: Check this later, i think and i remember the Trigger will no work with this.
+
+                    // Commmit the Transaction
                     transaction.Commit();
-                    return orderReceipt.OrderId; // Retornar el Id del OrderReceipt creado
+                    return orderReceipt.OrderId; 
                 }
                 catch (Exception ex)
                 {
                     transaction.Rollback();
-                    throw new ApplicationException("Error al crear el OrderReceipt y sus items", ex);
+                    return 0;
+                    //throw new ApplicationException("Error al crear el OrderReceipt y sus items", ex);
                 }
             }
            
         }
+        #endregion
 
+        // TODO: Try-Catch
         public async Task<List<OrderReceipt>> VerOrdenesRealizadas(string idEmpleado)
         {
-            // TODO: AutoMappers OrderReceiptDT
-            var query = context.OrderReceipts.Where(o => o.IdEmpleado == idEmpleado);
-            var ordenes = await query.ToListAsync();
-            return ordenes;
+            try
+            {
+                // TODO: AutoMappers OrderReceiptDT
+                var query = context.OrderReceipts.Where(o => o.IdEmpleado == idEmpleado);
+                var ordenes = await query.ToListAsync();
+                return ordenes;
+            }
+            catch
+            {
+                return null;
+            }
         }
-        public async Task<List<ProductItem>> VerOrderItems(int OrderId)
+
+        public async Task<List<ProductItemDTO>> VerOrderItems(int OrderId)
         {
-            // TODO: AutoMappers OrderReceiptDT
-            var query = context.ProductItems.Where(o => o.TicketOrderId == OrderId);
-            var ordenes = await query.ToListAsync();
-            return ordenes;
+            try
+            {
+                // TODO: AutoMappers OrderReceiptDT
+                var orderIdParameter = new SqlParameter("@OrderId", OrderId);
+
+                var productItems = await context.ProductItemsDTOs
+                    .FromSqlRaw(@"
+                            SELECT 
+                                IdItem, 
+                                (SELECT ProdName FROM Productos WHERE IdProducto = it.ProductID) AS ProductName, 
+                                Quantity, 
+                                UnitPrice, 
+                                ProductPriceID  
+                            FROM Product_Items it
+                            LEFT JOIN ProductPrices pp ON it.ProductID = pp.ProductID
+                            WHERE OrderId = @OrderId", orderIdParameter)
+                    .ToListAsync();
+
+                return productItems;
+            }
+            catch
+            {
+                return null;
+            }
         }
+
+
+
     }
 }

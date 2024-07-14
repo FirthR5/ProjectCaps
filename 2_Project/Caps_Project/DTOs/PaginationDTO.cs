8331792ba87ff61cfb06ee71215eeaacf20701a5
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-14 15:41:37 -0500

    Add: DataTableResponse & PaginationDTO Files

---
+++ 2_Project/Caps_Project/DTOs/PaginationDTO.cs
@@ -0,0 +1,16 @@
+﻿using Caps_Project.Models;
+
+namespace Caps_Project.DTOs
+{
+    public class PaginationDTO
+    {
+        public int pageSize { get; set; } = 0;
+        public int skip { get; set; } = 0;
+        public int recordsTotal { get; set; } = 0;
+    }
+    public class PaginationProductoDTO
+    {
+        public PaginationDTO paginationDTO { get; set; }
+        public List<Producto> listProductos { get; set; }
+    }
+}

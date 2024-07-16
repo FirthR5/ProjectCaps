8960d055948f096912b48602a01a08d457f25cd8
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-07-15 23:58:45 -0500

    Add: ProductoDTO

---
+++ 2_Project/Caps_Project/DTOs/OrdenDTOs/ProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Caps_Project.DTOs.OrdenDTOs
+{
+    public class ProductDTO
+    {
+        [Key]
+        public int IdProducto { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+
+        public int Stock { get; set; }
+
+    }
+}
